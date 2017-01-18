f749e0c30cd10a57e109300f67edf06251a530e8
Brad Baumel <Baumelbi@gmail.com>
2017-01-17 20:54:19 -0800

    running version of http_server and tests pass

--- session02/classwork/http_server.py
+++ session02/classwork/http_server.py
@@ -2,6 +2,24 @@ import socket
 import sys
 
 
+def response_method_not_allowed():
+    """returns a 405 Method Not Allowed response"""
+    resp = []
+    resp.append(b"HTTP/1.1 405 Method Not Allowed")
+    resp.append(b"")
+    return b"\r\n".join(resp)
+
+
+def response_ok():
+    """returns a basic HTTP response"""
+    resp = []
+    resp.append(b"HTTP/1.1 200 OK")
+    resp.append(b"Content-Type: text/plain")
+    resp.append(b"")
+    resp.append(b"this is a pretty minimal response")
+    return b"\r\n".join(resp)
+
+
 def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,22 +34,35 @@ def server(log_buffer=sys.stderr):
             conn, addr = sock.accept()  # blocking
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
+                request = ""
                 while True:
-                    data = conn.recv(16)
-                    print('received "{0}"'.format(data), file=log_buffer)
-                    if data:
-                        print('sending data back to client', file=log_buffer)
-                        conn.sendall(data)
-                    else:
-                        msg = 'no more data from {0}:{1}'.format(*addr)
-                        print(msg, log_buffer)
+                    data = conn.recv(1024)
+                    request += data.decode('utf8')
+                    if len(data) < 1024 or not data:
                         break
+                try:
+                    parse_request(request)
+                except NotImplementedError:
+                    response = response_method_not_allowed()
+                else:
+                    response = response_ok()
+
+                print('sending data back to client', file=log_buffer)
+                conn.sendall(response)
             finally:
                 conn.close()
 
     except KeyboardInterrupt:
         sock.close()
         return
+
+
+def parse_request(request):
+    first_line = request.split("\r\n", 1)[0]
+    method, uri, protocol = first_line.split()
+    if method != "GET":
+        raise NotImplementedError("We only accept GET")
+    print('request is okay', file=sys.stderr)
 
 
 if __name__ == '__main__':
